e27afc767bf8c75bf616fb30d763b7241610669c
Sean Eriksen <eriksensean@yahoo.com>
2019-10-23 21:21:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,4 +2,4 @@ reverse_each_word("Hello there, and how are you?")
 
 reverse_each_word("Hello", "there", ",", "and", "how", "are", "you", "?").reverse_each { |v| p v })
 
-reverse_each_word
+reverse_each_word("Hello there, and how are you?").collect { "cat"  }
